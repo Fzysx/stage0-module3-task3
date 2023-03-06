ccfaf3a83329f0066542e94a974d7fe129f333ac
Fzysx <44568244+Fzysx@users.noreply.github.com>
2023-03-06 19:42:51 +0600

    try to solve task s0m3t3

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,13 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        System.out.println(first);//write code here
+        System.out.println(second);//write code here
+        System.out.println(third);//write code here
+
     }
 }
